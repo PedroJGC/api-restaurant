d516fe08988ebe1dddb3ad67498d8f9c531a2884
Pedro Carvalho <pedro.juliogc02@gmail.com>
2025-06-10 19:11:55 -0300

    feat: implement error handling middleware and AppError class

--- src/controllers/products-controllers.ts
+++ src/controllers/products-controllers.ts
@@ -1,9 +1,10 @@
 import { Request, Response, NextFunction } from "express"
+import { AppError } from "@/utils/AppError"
 
 export class ProductController {
   async index(request: Request, response: Response, next: NextFunction) {
     try {
-      return response.json({ message: "OK" })
+      response.json({ message: "OK" })
     } catch (error) {
       next(error)
     }
