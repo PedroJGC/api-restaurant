bc881d3b7ac10249ad4f6694d3d679a23b897217
Pedro Carvalho <pedro.juliogc02@gmail.com>
2025-06-13 17:33:02 -0300

    feat: Implement product creation with Zod validation

--- src/controllers/products-controllers.ts
+++ src/controllers/products-controllers.ts
@@ -1,10 +1,25 @@
 import { Request, Response, NextFunction } from "express"
-import { AppError } from "@/utils/AppError"
+import { z } from "zod"
 
 export class ProductController {
   async index(request: Request, response: Response, next: NextFunction) {
     try {
       response.json({ message: "OK" })
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  async create(request: Request, response: Response, next: NextFunction) {
+    try {
+      const bodySchema = z.object({
+        name: z.string().trim().min(6),
+        price: z.number().gt(0, { message: "Price must be grater than 0!" }),
+      })
+
+      const { name, price } = bodySchema.parse(request.body)
+
+      response.status(201).json({ name, price })
     } catch (error) {
       next(error)
     }
