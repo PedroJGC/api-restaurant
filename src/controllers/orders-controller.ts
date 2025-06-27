e2df9d786e61a04a8f9906e0ef10d22e35610cf6
Pedro Carvalho <pedro.juliogc02@gmail.com>
2025-06-26 21:55:40 -0300

    feat: Order Business Rules

--- src/controllers/orders-controller.ts
+++ src/controllers/orders-controller.ts
@@ -1,4 +1,6 @@
 import { Request, Response, NextFunction } from "express"
+import { AppError } from "@/utils/AppError"
+import { knex } from "@/database/knex"
 import { z } from "zod"
 
 export class OrdersController {
@@ -14,7 +16,27 @@ export class OrdersController {
         request.body
       )
 
-      response.status(201).json()
+      const session = await knex<TablesSessionsRepository>("tables_sessions")
+        .where({ id: table_session_id })
+        .first()
+
+      if (!session) {
+        throw new AppError("Table session not found")
+      }
+
+      if (session.closed_at) {
+        throw new AppError("This table is closed")
+      }
+
+      const product = await knex<ProductRepository>("products")
+        .where({ id: product_id })
+        .first()
+
+      if (!product) {
+        throw new AppError("Product not found")
+      }
+
+      response.status(201).json(product)
     } catch (error) {
       next(error)
     }
